ac13c4c0e2901b19ab889e513d2cb4334288daa8
Paul <80376466+Super-Fusiom@users.noreply.github.com>
2025-09-25 17:25:15 +1200

    Basic function, docs for .env

---
+++ main.py
@@ -0,0 +1,26 @@
+import discord
+from dotenv import dotenv_values
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f"Logged in as {self.user}")
+
+    async def on_message(self, message):
+        print(f"Message from {message.author}: {message.content}")
+
+
+dotenv_values = dotenv_values(".env")
+bot_token: str = dotenv_values["BOT_TOKEN"]
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+
+def main() -> None:
+    client.run(dotenv_values["BOT_TOKEN"])
+
+
+if __name__ == "__main__":
+    main()
