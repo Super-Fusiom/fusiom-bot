760fa7cfdb5afc963fe4adcd82d2bab4473fe54c
Paul <80376466+Super-Fusiom@users.noreply.github.com>
2025-09-29 10:17:46 +1300

    Added, channel id. No way I'm using class based clients!

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
 import discord
 from dotenv import dotenv_values
 
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f"Logged in as {self.user}")
-
-    async def on_message(self, message):
-        print(f"Message from {message.author}: {message.content}")
-
-
 dotenv_values = dotenv_values(".env")
 bot_token: str = dotenv_values["BOT_TOKEN"]
-
+channel_id = dotenv_values["CHANNEL_ID"]
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = MyClient(intents=intents)
+
+client = discord.Client(intents=intents)
 
 
-# For python style coding -> Use main
+@client.event
+async def on_ready() -> None:
+    print(f"{client.user} has connected to Discord!")
+
+
+@client.event
+async def on_message(message: discord.Message) -> None:
+    if message.author == client.user:
+        return
+
+    if message.channel.id == int(channel_id):
+        await message.channel.send("Yurr")
+
+
 def main() -> None:
-    client.run(dotenv_values["BOT_TOKEN"])
+    client.run(bot_token)
 
 
 if __name__ == "__main__":
