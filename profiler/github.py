2c0f7c74c7b6d154a6ecefc28aac8e1a80b1c88e
Paul <80376466+Super-Fusiom@users.noreply.github.com>
2025-09-29 14:38:21 +1300

    Use a class rather then spread out functions.

--- profiler/github.py
+++ profiler/github.py
@@ -3,17 +3,37 @@ import requests
 
 class GithubUser:
     def __init__(self, username: str):
-        self._username = username
+        self._api_url = f"https://api.github.com/users/{username}"
+        self.username = username
+        self._repos: dict
+        self._followers: dict
+        self.__fetch_repos()
+        self.__fetch_followers()
+
+    @property
+    def repos(self):
+        return self._repos
+
+    @property
+    def followers(self):
+        return self._followers
+
+    def __fetch_repos(self):
+        api_url = f"{self._api_url}/repos"
+        response = requests.get(api_url)
+        data = response.json()
+        self._repos = data
+
+    def __fetch_followers(self):
+        api_url = f"{self._api_url}/followers"
+        response = requests.get(api_url)
+        data = response.json()
+        self._followers = data
+
+    def __str__(self):        
+        return f"{self.username} has {len(self._repos)} repos"
 
 
 def fetch_github_user(username: str):
-    api_url = f"https://api.github.com/users/{username}/repos"
-    response = requests.get(api_url)
-    data = response.json()
-    out = ""
-    num_of_repos = len(data)
-    if num_of_repos < 10:
-        out += f"This {username} is a donkey with {num_of_repos} repos"
-    else:
-        out += f"This {username} is a genius with {num_of_repos} repos"
-    return out
+    spied = GithubUser(username)
+    return f"This {spied.username} has {len(spied.repos)} repos"
